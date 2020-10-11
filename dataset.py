aed7cf5c611a3ccd4335ca299673a99d2bd44fa6
Hyun-Je Song <songhyunje@gmail.com>
2020-10-11 22:49:03 +0900

    Update lighting modules

--- dataset.py
+++ dataset.py
@@ -1,32 +1,62 @@
+from pathlib import Path
+import os
+
 import netCDF4 as nc
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
-from pathlib import Path
-
 
 class NowCastingDataset(Dataset):
-    def __init__(self, data_dir: str = './data', data_type: str = 'train'):
+    def __init__(self, data_dir: str = './data', data_type: str = 'train',
+                 prepare=False, seq_len: int = 6):
         super().__init__()
+        self.data_dir = data_dir
+        self.seq_len = seq_len
 
-        self.data = []
+        if prepare:
+            self.prepare(data_dir, data_type)
+
+        tmp = torch.load(os.path.join(self.processed_folder, data_type))
+        self.data = [tmp[i:i+self.seq_len] for i in range(len(tmp)+1-self.seq_len)]
+        # self.data.append(torch.stack(tmp))
+
+    def prepare(self, data_dir, data_type):
+        if self._check_exists(data_type):
+            return
+
+        print('Processing...')
+        tmp = []
         path = Path(data_dir + '/' + data_type)
         for x in path.iterdir():
             if x.is_dir():
                 files = sorted([f for f in x.iterdir() if f.is_file()])
-                tmp = []
                 for f in files:
                     ds = nc.Dataset(f)
                     value = ds.variables["data"][:]
                     tmp.append(torch.tensor(value))
-                self.data.append(torch.stack(tmp))
+
+        with open(os.path.join(self.processed_folder, data_type), 'wb') as f:
+            torch.save(tmp, f)
+
+        print('Done!')
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        return self.data[index][:-1], self.data[index][-1]
+        item = torch.stack(self.data[index])
+        # mask = self.data[index][-1].ge(0)
+        # return self.data[index][:-1], self.data[index][-1], mask
+        mask = item[1:].ge(0)
+        return item[:-1], item[1:], mask
+
+    @property
+    def processed_folder(self):
+        return os.path.join(self.data_dir, 'processed')
+
+    def _check_exists(self, data_type):
+        return os.path.exists(os.path.join(self.processed_folder, data_type))
 
 
 if __name__ == "__main__":
@@ -35,4 +65,5 @@ if __name__ == "__main__":
     for d in dataloader:
         print(d[0].size())
         print(d[1].size())
+        print(d[2].size())
         break
