90657c96eae32d3d9b7b875c449a835e3be8d8df
Hyun-Je Song <songhyunje@gmail.com>
2020-10-11 22:57:46 +0900

    Revert "Update lighting modules"
    This reverts commit aed7cf5c

--- data_module.py
+++ data_module.py
@@ -10,12 +10,9 @@ class NowCastingDataModule(pl.LightningDataModule):
         self.data_dir = data_dir
         self.batch_size = batch_size
 
-    def prepare_data(self):
-        # Download, tokenize, etc
-        # Write to disk or that need to be done only from a single GPU in distributed settings
-        NowCastingDataset(self.data_dir, data_type='train', prepare=True)
-        NowCastingDataset(self.data_dir, data_type='valid', prepare=True)
-        NowCastingDataset(self.data_dir, data_type='test', prepare=True)
+    # def prepare_data(self):
+    #     # Download, tokenize, etc
+    #     # Write to disk or that need to be done only from a single GPU in distributed settings
 
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
@@ -26,18 +23,17 @@ class NowCastingDataModule(pl.LightningDataModule):
             self.test = NowCastingDataset(self.data_dir, data_type='test')
 
     def train_dataloader(self):
-        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True, num_workers=4)
 
     def val_dataloader(self):
-        return DataLoader(self.valid, batch_size=self.batch_size)
+        return DataLoader(self.valid, batch_size=self.batch_size, num_workers=4)
 
     def test_dataloader(self):
-        return DataLoader(self.test, batch_size=self.batch_size)
+        return DataLoader(self.test, batch_size=self.batch_size, num_workers=4)
 
 
 if __name__ == "__main__":
     data_module = NowCastingDataModule()
-    data_module.prepare_data()
     data_module.setup('fit')
     for batch in data_module.train_dataloader():
         print(batch[0].size())
--- trainer.py
+++ trainer.py
@@ -1,48 +1,29 @@
 import argparse
-import os
-from pathlib import Path
 
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
 
 from data_module import NowCastingDataModule
 from model import LightningModel
 
 
 def train(args):
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        filepath=args.output_dir,
-        prefix="checkpoint",
-        monitor="val_loss",
-        verbose=True,
-        mode="min",
-        save_top_k=3
-    )
+    # if os.path.exists(args.output_dir) and os.listdir(args.output_dir) \
+    #         and args.do_train and not args.overwrite_output:
+    #     raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
+    #
+    # checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    #     filepath=args.output_dir,
+    #     prefix="checkpoint",
+    #     monitor="val_loss",
+    #     verbose=True,
+    #     mode="min",
+    #     save_top_k=3
+    # )
 
-    output_dir = Path(args.output_dir)
-    output_dir.mkdir(exist_ok=True)
-
-    # default logger used by trainer
-    logger = TensorBoardLogger(
-        save_dir=os.getcwd(),
-        version=1,
-        name='lightning_logs'
-    )
-
-    pl.seed_everything(args.seed)
     data_module = NowCastingDataModule(data_dir=args.data_dir, batch_size=2)
-
-    train_params = {}
-    if args.gpus > 1:
-        train_params["distributed_backend"] = "ddp"
-
-    train_params["accumulate_grad_batches"] = args.accumulate_grad_batches
-
     model = LightningModel(args)
-    trainer = pl.Trainer.from_argparse_args(args,
-                                            checkpoint_callback=checkpoint_callback,
-                                            logger=logger,
-                                            **train_params)
+    # trainer = pl.Trainer(gpus=2, distributed_backend='dp', max_epochs=100)
+    trainer = pl.Trainer(gpus=1, max_epochs=100)
 
     trainer.fit(model, data_module)
 
@@ -54,28 +35,10 @@ def train(args):
 def add_generic_arguments(parser):
     parser.add_argument("--data_dir", default=None, type=str, required=True,
                         help="The input data dir.")
-    parser.add_argument("--output_dir", default=None, type=str, required=True,
-                        help="The output directory where the model predictions and checkpoints will be written.",
-                        )
-    parser.add_argument("--fast_dev_run", action="store_true")
-    parser.add_argument("--gpus", type=int)
-    parser.add_argument("--gradient_clip_val", default=5.0, type=float, help="Gradient clipping value")
-    parser.add_argument("--precision", default=32, type=int, help="Precision")
-    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
-    parser.add_argument("--do_predict", action="store_true", help="Whether to run predictions on the test set.")
-    parser.add_argument("--accumulate_grad_batches", type=int, default=1,
-                        help="Number of updates steps to accumulate before performing a backward/update pass.",
-                        )
-    parser.add_argument("--sync_batchnorm", action="store_true",
-                        help="Enable synchronization between batchnorm layers across all GPUs.")
-    parser.add_argument("--seed", type=int, default=43, help="random seed for initialization")
-    parser.add_argument("--resume_from_checkpoint", type=str, default=None)
-    parser.add_argument("--val_check_interval", default=1.0, type=float)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     add_generic_arguments(parser)
-    LightningModel.add_model_specific_args(parser)
     args = parser.parse_args()
     train(args)
