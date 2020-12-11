1a504f8d5739ac65b3a28842c795021a71825da5
Hyun-Je Song <songhyunje@gmail.com>
2020-12-11 15:38:30 +0000

    add step

--- predict.py
+++ predict.py
@@ -26,31 +26,39 @@ def prediction(args):
 
     with torch.no_grad():
         for data in prediction_dataloader:
-            fn, seqs, target, mask = data['fn'], data['seqs'], data['target'], data['mask']
+            fn, seqs, mask = data['fn'], data['seqs'], data['mask']
             seqs = seqs.to(device)
             mask = mask.to(device)
-            target = target.to(device)
+            # target = data['target'] 
+            # target = target.to(device)
 
-            pred = model.infer(seqs)
-            # print(F.mse_loss(pred, target, reduction='sum'))
+            for i in range(args.step):
+                pred = model.infer(seqs)
+                pred[pred < (1/250)] = 0   
+                seqs = torch.cat([seqs, pred.unsqueeze(0)], dim=1)
 
-            pred *= 250
-            pred[pred < 1] = 0
-            # print(pred)
+                pred *= 250
+                pred[pred < 1] = 0
+                # pred = torch.where(mask < 0, mask, pred)
+                pred.masked_fill_(mask, -9999)
+                path = Path(args.output_dir + '/' + fn[0].replace('/', '_') + '_' + str(i+1))
+                np.save(path, pred[0].cpu().numpy())
+ 
+            # pred *= 250
+            # pred[pred < 1] = 0
             # pred = torch.where(mask < 0, mask, pred)
-            pred.masked_fill_(mask, -9999)
-            # print(pred)
-            # print(torch.sum(pred[pred > 0]))
-            # print(torch.sum(target[target > 0]))
+            # pred.masked_fill_(mask, -9999)
+            
+            # path = Path(args.output_dir + '/' + fn[0].replace('/', '_'))
+            # np.save(path, pred[0].cpu().numpy())
 
-            path = Path(args.output_dir + '/' + fn[0].replace('/', '_'))
-            np.save(path, pred[0].cpu().numpy())
 
 
 def add_generic_arguments(parser):
     parser.add_argument("--data_dir", default=None, type=str, required=True)
     parser.add_argument("--output_dir", default=None, type=str, required=True)
     parser.add_argument("--checkpoint", default=None, type=str, required=True)
+    parser.add_argument("--step", default=1, type=int)
 
 
 if __name__ == "__main__":
